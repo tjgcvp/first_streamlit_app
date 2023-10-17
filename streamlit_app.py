c98a3ce4e4652968bf6b8fef244ba72becd79b24
tjgcvp <148173770+tjgcvp@users.noreply.github.com>
2023-10-16 22:52:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
-streamlit.subheader('Omega 3 & Blueberry Oatmeal',
-               'kale , Spinach & Rocket Smoothie',
+streamlit.title('Omega 3 & Blueberry Oatmeal'
+               'kale , Spinach & Rocket Smoothie'
                'Hard-boiled Free-range Egg')
