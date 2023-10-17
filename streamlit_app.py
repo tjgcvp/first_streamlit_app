c14023297d6eced0f5b6be4e8f8db4dca0110966
tjgcvp <148173770+tjgcvp@users.noreply.github.com>
2023-10-16 22:55:50 -0400

    Update streamlit_app.py
    added Breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale , Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-range Egg')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
