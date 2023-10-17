a1f8b713920411b641551c17b4f40e0202a1a3ac
tjgcvp <148173770+tjgcvp@users.noreply.github.com>
2023-10-16 23:15:01 -0400

    Update streamlit_app.py
    default fruit selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocda Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),[Avocado, Strawberries)
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
