1d438484e3159ec0cceb7b5b4953b1c018171853
tjgcvp <148173770+tjgcvp@users.noreply.github.com>
2023-10-17 17:36:15 -0400

    Update streamlit_app.py
    added fruit variable instead of hardcode value

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
